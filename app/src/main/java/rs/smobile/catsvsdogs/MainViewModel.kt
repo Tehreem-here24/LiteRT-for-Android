e381d26633778ab7a08d1aaa487e5df139662e04
Stevan Milovanovic <stevan.milovanovic@twint.ch>
2025-05-19 00:51:32 +0200

    Initial commit

---
+++ app/src/main/java/rs/smobile/catsvsdogs/MainViewModel.kt
@@ -0,0 +1,53 @@
+package rs.smobile.catsvsdogs
+
+import android.graphics.Bitmap
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import rs.smobile.catsvsdogs.classifier.ImageClassifier
+import rs.smobile.catsvsdogs.data.local.model.Image
+import rs.smobile.catsvsdogs.data.repository.ImageRepository
+import rs.smobile.catsvsdogs.di.CatImageRepo
+import rs.smobile.catsvsdogs.di.DogImageRepo
+import javax.inject.Inject
+import kotlin.random.Random
+
+@HiltViewModel
+class MainViewModel @Inject constructor(
+    @DogImageRepo private val dogImageRepository: ImageRepository,
+    @CatImageRepo private val catImageRepository: ImageRepository,
+    private val imageClassifier: ImageClassifier,
+) : ViewModel() {
+
+    private val _image = MutableStateFlow<Image?>(null)
+    val image: StateFlow<Image?> = _image
+
+    private val _expectedLabel = MutableStateFlow<String>("")
+    val expectedLabel: StateFlow<String> = _expectedLabel
+
+    private val _classifiedLabel = MutableStateFlow<String>("")
+    val classifiedLabel: StateFlow<String> = _classifiedLabel
+
+    fun loadRandomImage() {
+        viewModelScope.launch {
+            _expectedLabel.value = ""
+            _classifiedLabel.value = ""
+
+            _image.value = if (Random.Default.nextBoolean()) {
+                _expectedLabel.value = "Dog"
+                dogImageRepository.getRandomImage()
+            } else {
+                _expectedLabel.value = "Cat"
+                catImageRepository.getRandomImage()
+            }
+        }
+    }
+
+    fun inferImageClass(bitmap: Bitmap) {
+        _classifiedLabel.value = imageClassifier.recognizeImage(bitmap).first().title
+    }
+
+}
